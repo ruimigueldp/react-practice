a9e57b1637a93882f38d886dec490a612f4186d1
Rui Paredes <ruimigueldp0@gmail.com>
2018-04-30 00:01:23 +0100

    Create branch to test react-transitions-group

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Transition from 'react-transition-group/Transition';
+
+import "./Modal.css";
+
+const modal = props => {
+  return (
+    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+      {state => {
+        const cssClasses = [
+          "Modal",
+          state === 'entering'
+          ? "ModalOpen"
+          : state === 'exiting' ? "ModalClosed" : null
+        ];
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
+};
+
+export default modal;
